b8f5fff5828a61cd97b49d22af78c30ab36f26d4
Wils McCreight <wizcon@me.com>
2019-09-27 02:13:21 -0400

    Add time output data

--- computation/main.cpp
+++ computation/main.cpp
@@ -10,41 +10,119 @@
 using namespace std;
 
 int main () {
-  int num_arrays = 50;
-  int array_size = 100;
+  int num_arrays = 20;
+  int array_size = 10;
   clock_t time_start;
   ofstream myfile;
   myfile.open("outputs/data/bubble_sort_times.csv");
-  myfile << "Row, bub_rand_small, bub_rand_med, bub_rand_large, bub_asc_small, bub_asc_med, bub_asc_large, bub_desc_small, bub_desc_med, bub_desc_large\n";
+  myfile << "ID, bub_rand_10, bub_rand_100, bub_rand_1000, bub_rand_10000, bub_asc_10, bub_asc_100, bub_asc_1000, bub_asc_10000, bub_desc_10, bub_desc_100, bub_desc_1000, bub_desc_1000\n";
 
+  vector<clock_t> bub_rand_timesI, bub_rand_timesII, bub_rand_timesIII, bub_rand_timesIV;
+  vector<clock_t> bub_asc_timesI, bub_asc_timesII, bub_asc_timesIII, bub_asc_timesIV;
+  vector<clock_t> bub_desc_timesI,  bub_desc_timesII,  bub_desc_timesIII,  bub_desc_timesIV;
+  bub_rand_timesI.resize(num_arrays);
+  bub_rand_timesII.resize(num_arrays);
+  bub_rand_timesIII.resize(num_arrays);
+  bub_rand_timesIV.resize(num_arrays);
+  bub_asc_timesI.resize(num_arrays);
+  bub_asc_timesII.resize(num_arrays);
+  bub_asc_timesIII.resize(num_arrays);
+  bub_asc_timesIV.resize(num_arrays);
+  bub_desc_timesI.resize(num_arrays);
+  bub_desc_timesII.resize(num_arrays);
+  bub_desc_timesIII.resize(num_arrays);
+  bub_desc_timesIV.resize(num_arrays);
 
-  vector<clock_t> bub_rand_times, ins_rand_times, sel_rand_times;
-  bub_rand_times.resize(num_arrays);
-  sel_rand_times.resize(num_arrays);
-  ins_rand_times.resize(num_arrays);
+  Arr rand_arr, asc_arr, desc_arr, sorted_arr;
 
-  Arr rand_arr, sorted_arr;
-
+  //Run bubble sort tests (10)
   for(int i = 0; i < num_arrays; i++) {
     rand_arr = Arr(array_size, "random");
-    // Arr asc_arr = Arr(array_size, "ascending");
-    // Arr desc_arr = Arr(array_size, "descending");
+    asc_arr = Arr(array_size, "ascending");
+    desc_arr = Arr(array_size, "descending");
 
     time_start = clock();
     sorted_arr = bubble_sort(rand_arr);
-    bub_rand_times[i] = time_start - clock();
+    bub_rand_timesI[i] = time_start - clock();
 
     time_start = clock();
-    sorted_arr = insertion_sort(rand_arr);
-    ins_rand_times[i] = time_start - clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_asc_timesI[i] = time_start - clock();
 
     time_start = clock();
-    sorted_arr = selection_sort(rand_arr);
-    sel_rand_times[i] = time_start - clock();
-
-    myfile.close();
+    sorted_arr = bubble_sort(desc_arr);
+    bub_desc_timesI[i] = time_start - clock();
   }
 
+  array_size = array_size * 10;
+
+  for(int i = 0; i < num_arrays; i++) {
+    rand_arr = Arr(array_size, "random");
+    asc_arr = Arr(array_size, "ascending");
+    desc_arr = Arr(array_size, "descending");
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_rand_timesII[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_asc_timesII[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(desc_arr);
+    bub_desc_timesII[i] = time_start - clock();
+  }
+
+  array_size = array_size * 10;
+
+  for(int i = 0; i < num_arrays; i++) {
+    rand_arr = Arr(array_size, "random");
+    asc_arr = Arr(array_size, "ascending");
+    desc_arr = Arr(array_size, "descending");
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_rand_timesIII[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_asc_timesIII[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(desc_arr);
+    bub_desc_timesIII[i] = time_start - clock();
+  }
+
+  array_size = array_size * 10;
+
+  for(int i = 0; i < num_arrays; i++) {
+    rand_arr = Arr(array_size, "random");
+    asc_arr = Arr(array_size, "ascending");
+    desc_arr = Arr(array_size, "descending");
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_rand_timesIV[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(rand_arr);
+    bub_asc_timesIV[i] = time_start - clock();
+
+    time_start = clock();
+    sorted_arr = bubble_sort(desc_arr);
+    bub_desc_timesIV[i] = time_start - clock();
+  }
+
+
+
+  for(int k = 0; k < num_arrays; k++) {
+    myfile << k << ", " << ((float)bub_rand_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIV[k])/CLOCKS_PER_SEC;
+    myfile << ", " << ((float)bub_asc_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesIV[k])/CLOCKS_PER_SEC;
+    myfile << ", " << ((float)bub_desc_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesIV[k])/CLOCKS_PER_SEC << "\n";
+  }
+
+  myfile.close();
   // cout << bub_rand_times;
   // time_end
   // time_start
