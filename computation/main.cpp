33a90a853b13db8e0edf89c01e3c25f334d621b7
Wils McCreight <wizcon@me.com>
2019-10-01 15:48:51 -0400

    Cleaned up comments

--- computation/main.cpp
+++ computation/main.cpp
@@ -123,44 +123,6 @@ int main () {
   }
 
   myfile.close();
-  // cout << bub_rand_times;
-  // time_end
-  // time_start
-  // bubble_sort(desc_arr);
-  // time_end
-  // time_start
-  // bubble_sort(rand_arr);
-  // time_end
-
-  // time_start
-  // selection_sort(asc_arr);
-  // time_end
-  // time_start
-  // selection_sort(desc_arr);
-  // time_end
-  // time_start
-  // selection_sort(rand_arr);
-  // time_end
-
-  // time_start
-  // insertion_sort(asc_arr);
-  // time_end
-  // time_start
-  // insertion_sort(desc_arr);
-  // time_end
-  // time_start
-  // insertion_sort(rand_arr);
-  // time_end
-
-  // time_start
-  // quick_sort(asc_arr);
-  // time_end
-  // time_start
-  // quick_sort(desc_arr);
-  // time_end
-  // time_start
-  // quick_sort(rand_arr);
-  // time_end
 
   return 0;
 }
