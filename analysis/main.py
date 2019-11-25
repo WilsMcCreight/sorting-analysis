1d51777f35199902a569e4b19906e5fe4ead5d60
Wils McCreight <wizcon@me.com>
2019-11-24 22:24:34 -0500

    Added verbose output

--- analysis/main.py
+++ analysis/main.py
@@ -1,8 +1,18 @@
+import pandas as pd
+import sys
 
 
+def create_dataframe(path):
 
-def foo():
-    print("Foo was called")
+    data = pd.read_csv(path)
+    print(data)
 
 if __name__ == "__main__":
-    foo()
+
+    create_dataframe(sys.argv[1])
+    # 
+    # dict = {}
+    # dict["two"] = 2
+    # print(dict)
+    # if "one" in dict.keys():
+    #     print(dict["one"])
--- computation/main.cpp
+++ computation/main.cpp
@@ -7,16 +7,16 @@
 #include "algorithms/bubble_sort.h"
 #include "algorithms/insertion_sort.h"
 #include "algorithms/selection_sort.h"
-using namespace std;
+// using namespace std;
 
 int main () {
-  int num_arrays = 20;
-  int array_size = 10;
-  int size_scaler = 10;
+  int num_arrays = 3;
+  int array_size = 1;
+  int size_scaler = 100;
   clock_t time_start;
   ofstream myfile;
   myfile.open("outputs/data/bubble_sort_times.csv");
-  myfile << "ID, bub_rand_10, bub_rand_100, bub_rand_1000, bub_rand_10000, bub_asc_10, bub_asc_100, bub_asc_1000, bub_asc_10000, bub_desc_10, bub_desc_100, bub_desc_1000, bub_desc_1000\n";
+  myfile << "bub_rand_1, bub_rand_100, bub_rand_10000, bub_rand_1000000, bub_asc_1, bub_asc_100, bub_asc_10000, bub_asc_1000000, bub_desc_1, bub_desc_100, bub_desc_10000, bub_desc_1000000\n";
 
   vector<clock_t> bub_rand_timesI, bub_rand_timesII, bub_rand_timesIII, bub_rand_timesIV;
   vector<clock_t> bub_asc_timesI, bub_asc_timesII, bub_asc_timesIII, bub_asc_timesIV;
@@ -36,8 +36,12 @@ int main () {
 
   Arr rand_arr, asc_arr, desc_arr, sorted_arr;
 
+  cout << "Starting Round 1\n";
+
   //Run bubble sort tests (10)
   for(int i = 0; i < num_arrays; i++) {
+    cout << "Round 1 Array " << i << "\n";
+
     rand_arr = Arr(array_size, "random");
     asc_arr = Arr(array_size, "ascending");
     desc_arr = Arr(array_size, "descending");
@@ -55,9 +59,13 @@ int main () {
     bub_desc_timesI[i] = time_start - clock();
   }
 
+  cout << "Starting Round 2\n";
+
   array_size = array_size * size_scaler;
 
   for(int i = 0; i < num_arrays; i++) {
+    cout << "Round 2 Array " << i << "\n";
+
     rand_arr = Arr(array_size, "random");
     asc_arr = Arr(array_size, "ascending");
     desc_arr = Arr(array_size, "descending");
@@ -75,9 +83,13 @@ int main () {
     bub_desc_timesII[i] = time_start - clock();
   }
 
+  cout << "Starting Round 3\n";
+
   array_size = array_size * size_scaler;
 
   for(int i = 0; i < num_arrays; i++) {
+    cout << "Round 3 Array " << i << "\n";
+
     rand_arr = Arr(array_size, "random");
     asc_arr = Arr(array_size, "ascending");
     desc_arr = Arr(array_size, "descending");
@@ -95,9 +107,13 @@ int main () {
     bub_desc_timesIII[i] = time_start - clock();
   }
 
+  cout << "Starting Round 4\n";
+
   array_size = array_size * size_scaler;
 
   for(int i = 0; i < num_arrays; i++) {
+    cout << "Round 4 Array " << i << "\n";
+
     rand_arr = Arr(array_size, "random");
     asc_arr = Arr(array_size, "ascending");
     desc_arr = Arr(array_size, "descending");
@@ -115,14 +131,18 @@ int main () {
     bub_desc_timesIV[i] = time_start - clock();
   }
 
+  cout << "Writing to memory\n";
+
 
   for(int k = 0; k < num_arrays; k++) {
-    myfile << k << ", " << ((float)bub_rand_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIV[k])/CLOCKS_PER_SEC;
+    myfile << ((float)bub_rand_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_rand_timesIV[k])/CLOCKS_PER_SEC;
     myfile << ", " << ((float)bub_asc_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_asc_timesIV[k])/CLOCKS_PER_SEC;
     myfile << ", " << ((float)bub_desc_timesI[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesIII[k])/CLOCKS_PER_SEC << ", " << ((float)bub_desc_timesIV[k])/CLOCKS_PER_SEC << "\n";
   }
 
   myfile.close();
+
+  cout << "Done!\n";
 
   return 0;
 }
